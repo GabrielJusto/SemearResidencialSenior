41be1e94bc64a41cb4ed6448d03d733857128c68
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2023-09-21 15:46:14 -0300

    adicionado carrossel de imagens

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ function abreEquipe(div){
     else	
         document.getElementById(div).style.display = 'block'
 }
+
+let slideIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.carousel-slide');
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = index;
+  }
+
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${-slideIndex * 25}vw)`;
+  });
+}
+
+function prevSlide() {
+  showSlide(slideIndex - 1);
+}
+
+function nextSlide() {
+  showSlide(slideIndex + 1);
+}
+
+// Mostra o primeiro slide ao carregar a página
+showSlide(slideIndex);
