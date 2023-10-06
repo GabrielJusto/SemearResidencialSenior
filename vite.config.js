29d49b5d0830ce09402476bc8405429217b62a79
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2023-10-05 23:07:37 -0300

    voltando para http

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port:80,
-    https: true,
     host: true,
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
@@ -13,6 +11,6 @@ export default defineConfig({
         : undefined,
   },
   plugins: [
-    basicSsl()
+   
   ]
 });
